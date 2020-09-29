e99b638c64b28c387e813bc6c8de59e0456c2977
Phil Eagleson <phil@iMac.local>
2020-09-28 22:54:47 -0400

    fixed css typo - missing semi

--- gatsby/src/styles/Grids.js
+++ gatsby/src/styles/Grids.js
@@ -2,8 +2,9 @@ import styled from 'styled-components'
 
 export const HomePageGrid = styled.div`
   display: grid;
-  gap: 2rem;
-  --columns: 2
+  row-gap: 2rem;
+  column-gap: 2rem;
+  --columns: 2;
   grid-template-columns: repeat(var(--columns), minmax(auto, 1fr));
 
   @media (max-width: 800px) {
